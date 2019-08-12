ross-spencer/python-gearman

commit 0eb378ec5c8ab5163d4311e82c4b445ddcb172f6

Get the admin_client_tests passing

gearman/connection.py
@@ -56,7 +56,7 @@ class GearmanConnection(object):
         self._is_server_side = None
 
         # Reset all our raw data buffers
-        self._incoming_buffer = array.array('c')
+        self._incoming_buffer = array.array("b")
         self._outgoing_buffer = ''
 
         # Toss all commands we may have sent or received

commit dcf4d7ef69bc4e280838330ba9c2dcdd7a7516af

Correct int => byte comparison
0eb378e introduced a breaking change to the Py2 gearman library. A
char array was changed to a signed char (integer) array, but the
comparison looking for `\x00` at the beginning of this array was not
updated to compare int to byte. This is corrected here and should be
compatible between Python 2 and 3.

gearman/connection.py
@@ -208,7 +208,7 @@ class GearmanConnection(object):
             cmd_type = None
             cmd_args = None
             cmd_len = 0
-        elif given_buffer[0] == NULL_CHAR:
+        elif struct.pack("B", given_buffer[0]) == NULL_CHAR:
             # We'll be expecting a response if we know we're a client side command
             is_response = bool(self._is_client_side)
             cmd_type, cmd_args, cmd_len = parse_binary_command(given_buffer, is_response=is_response)